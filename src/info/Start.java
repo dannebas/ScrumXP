771e45711c5377af2966a5b2b848dc5aae68ac25
Daniel Lindau <daniel.s.lindau@gmail.com>
2020-04-14 09:28:48 +0200

    new commit

---
+++ src/info/Start.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info;
+
+import dbUtils.dbConnection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Start {
+
+    private static dbConnection conn;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+
+        conn = new dbConnection();
+
+        System.out.println(conn.fetchSingle("SELECT USER_ID, PASSWORD from USER"));
+
+        new LogInGUI().setVisible(true);
+    }
+}
